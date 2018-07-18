be09062dd81dd6875074552493cd758393e2118c
Matt DiCarlo <dicarlo.matt@gmail.com>
2018-07-18 08:30:12 -0500

    Added personal keymap for lfk78.

---
+++ keyboards/lfkeyboards/lfk78/keymaps/mattdicarlo/keymap.c
@@ -0,0 +1,150 @@
+#include QMK_KEYBOARD_H
+
+// readability
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+enum keymap_layout {
+    BASE = 0,     // Base layout, Mac OS modifiers
+    FN,           // Function layer for Mac OS layout
+    WIN,          // Toggled Windows layout
+    WFN,          // Function layer for Windows layout
+};
+
+// Colors of the layer indicator LED
+// This list needs to define layer 0xFFFFFFFF, it is the end of the list, and the unknown layer
+const Layer_Info layer_info[] = {
+  // Layer     Mask           Red     Green   Blue
+  {0x00000000, 0xFFFFFFFF, {0x05DF, 0x0CEF, 0x0BAF}}, // base layer
+  {0x00000002, 0xFFFFFFFE, {0x06CF, 0x03BF, 0x07BF}}, // function layer
+  {0x00000004, 0xFFFFFFFC, {0x0FFF, 0x0000, 0x0FFF}}, // windows layer
+  {0x00000006, 0xFFFFFFF7, {0x0FFF, 0x0000, 0x0000}}, // windows function layer
+
+  {0xFFFFFFFF, 0xFFFFFFFF, {0x0FFF, 0x0FFF, 0x0FFF}}, // unknown layer - REQUIRED - white
+};
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap BASE: Default Layer
+   * ,---------.  ,------------------------------------------------------------.  ,---------.
+   * |    |    |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa|  | `  |Home|
+   * |---------|  |------------------------------------------------------------|  |---------|
+   * |    |    |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     \|  | Del|End |
+   * |---------|  |------------------------------------------------------------|  `---------'
+   * |    |    |  |CAPS    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |---------|  |------------------------------------------------------------|  ,----.
+   * |    |    |  |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift      |  | Up |
+   * |---------|  |-------------------------------------------------------------------------.
+   * |    |    |  |Ctrl|Alt |Cmd |      Space            |Cmd |Func|Ctrl |    |Lft| Dn |Rig |
+   * `---------'  `------------------------------------------------------'    `-------------'
+   */
+  [BASE] = LAYOUT(
+    XXXXXXX, XXXXXXX,  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,          KC_GRV,  KC_HOME, \
+    XXXXXXX, XXXXXXX,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,          KC_DEL,  KC_END,  \
+    XXXXXXX, XXXXXXX,  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,                             \
+    XXXXXXX, XXXXXXX,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSFT,          KC_UP,            \
+    XXXXXXX, XXXXXXX,  KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RGUI, MO(FN) , KC_RCTL,           KC_LEFT, KC_DOWN, KC_RGHT  \
+  ),
+
+  /* Keymap FN: Function Layer
+   * ,---------.  ,-------------------------------------------------------------.  ,---------.
+   * |    |    |  | ` |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Delete   |  |    |PgUp|
+   * |---------|  |-------------------------------------------------------------|  |---------|
+   * |    |    |  |WIN  |   | Up|   |   |   |   |   |   |   |   |   |   |       |  |Mute|PgDn|
+   * |---------|  |-------------------------------------------------------------|  `---------'
+   * |    |    |  |       |Lft|Dn |Rig|   |   |Lft|Dwn| Up|Rgt|   |   |         |
+   * |---------|  |-------------------------------------------------------------|  ,----.
+   * |    |    |  |         |   |   |   |   |   |   |    |   |   |   |          |  |Vol+|
+   * |---------|  |--------------------------------------------------------------------------.
+   * |    |    |  |    |    |    |      Play/Pause        |    |Fn  |    |    |Prev|Vol-|Next|
+   * `---------'  `------------------------------------------------------'    `--------------'
+   */
+  [FN] = LAYOUT(
+    XXXXXXX, XXXXXXX, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           XXXXXXX, KC_PGUP, \
+    XXXXXXX, XXXXXXX, TG(WIN), XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_MUTE, KC_PGDN,  \
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,          XXXXXXX,                            \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                   XXXXXXX,          KC_VOLU,          \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_MPLY,                            XXXXXXX, _______, XXXXXXX,          KC_MRWD, KC_VOLD, KC_MFFD  \
+  ),
+
+  /* Keymap WIN: Toggleable Windows layout
+   * ,---------.  ,------------------------------------------------------------.  ,---------.
+   * |    |    |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa|  | `  |Home|
+   * |---------|  |------------------------------------------------------------|  |---------|
+   * |    |    |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     \|  | Del|End |
+   * |---------|  |------------------------------------------------------------|  `---------'
+   * |    |    |  |CAPS    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |---------|  |------------------------------------------------------------|  ,----.
+   * |    |    |  |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift      |  | Up |
+   * |---------|  |-------------------------------------------------------------------------.
+   * |    |    |  |Ctrl|Win |Alt |      Space            |Alt |Fn  |Ctrl |    |Lft| Dn |Rig |
+   * `---------'  `------------------------------------------------------'    `-------------'
+   */
+  [WIN] = LAYOUT(
+    XXXXXXX, XXXXXXX,  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,   KC_BSPC,          KC_GRV,  KC_HOME, \
+    XXXXXXX, XXXXXXX,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,          KC_DEL,  KC_END,  \
+    XXXXXXX, XXXXXXX,  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,                             \
+    XXXXXXX, XXXXXXX,  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSFT,          KC_UP,            \
+    XXXXXXX, XXXXXXX,  KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(WFN), KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
+  ),
+
+  /* Keymap WMN: Function Layer for Windows Layout
+   * ,---------.  ,-------------------------------------------------------------.  ,---------.
+   * |    |    |  |`  |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Delete   |  |    |PgUp|
+   * |---------|  |-------------------------------------------------------------|  |---------|
+   * |    |    |  |BASE |   | Up|   |   |   |   |   |   |   |   |   |   |       |  |Mute|PgDn|
+   * |---------|  |-------------------------------------------------------------|  `---------'
+   * |    |    |  |       |Lft|Dn |Rig|   |   |Lft|Dwn| Up|Rgt|   |   |         |
+   * |---------|  |-------------------------------------------------------------|  ,----.
+   * |    |    |  |         |   |   |   |   |   |   |    |   |   |   |          |  |Vol+|
+   * |---------|  |--------------------------------------------------------------------------.
+   * |    |    |  |    |    |    |      Play/Pause        |    |Fn  |    |    |Prev|Vol-|Next|
+   * `---------'  `------------------------------------------------------'    `--------------'
+   */
+  [WFN] = LAYOUT(
+    XXXXXXX, XXXXXXX, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           XXXXXXX, KC_PGUP, \
+    XXXXXXX, XXXXXXX, KC_FN0,  XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_MUTE, KC_PGDN,  \
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,          XXXXXXX,                            \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX,                   XXXXXXX,          KC_VOLU,          \
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            KC_MPLY,                            XXXXXXX, _______, XXXXXXX,          KC_MRWD, KC_VOLD, KC_MFFD  \
+  ),
+
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    ACTION_FUNCTION(LFK_CLEAR),                               // FN0 - reset layers
+    ACTION_FUNCTION(LFK_CLICK_FREQ_HIGHER),                   // FN1 - Increase Freq of audio click
+    ACTION_FUNCTION(LFK_CLICK_FREQ_LOWER),                    // FN2 - Decrease Freq of audio click
+    ACTION_FUNCTION(LFK_CLICK_TIME_LONGER),                   // FN3 - Increase length of audio click
+    ACTION_FUNCTION(LFK_CLICK_TIME_SHORTER),                  // FN4 - Decrease length of audio click
+    ACTION_FUNCTION(LFK_CLICK_TOGGLE),                        // FN5 - Toggle audio click
+};
+
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+      }
+    return MACRO_NONE;
+};
+
+
+void matrix_init_user(void) {
+    // This keymap only has a single base layer, so reset the default if needed
+    if(eeconfig_read_default_layer() > 1){
+        eeconfig_update_default_layer(1);
+        default_layer_set(1);
+    }
+}
+
+void matrix_scan_user(void) {
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
+
+void led_set_user(uint8_t usb_led) {
+}
---
+++ keyboards/lfkeyboards/lfk78/keymaps/mattdicarlo/readme.md
@@ -0,0 +1,19 @@
+# Matt's LFK78 Hotswap Board
+
+LFK78/68-HS Rev B PCB (at90usb646)
+
+Mac OS modifiers, rearranged default four-square block from the MF68, customized function layer.
+
+Uses Grave/Esc on the Esc key so that Cmd+Esc works to cycle through application windows as if the Grave/Tilde key was in the usual place, and sets the `GRAVE_ESC_ALT_OVERRIDE` flag so that Cmd-Opt-Esc still opens the Force Quit menu.
+
+Fn-Tab toggles a Windows layout, which is basically the same except with Alt & Cmd reversed the way that Windows expects.
+
+LED colors are based off of the [SA Hyperfuse color palette][hf], translating from [SP color chip codes][sp] to RGB:
+
+Teal:       VBV     #5dceba
+Purple:     RDA     #6c3b7b
+Light Grey: GTL
+Dark Grey:  GD
+
+[hf]:https://www.reddit.com/r/MechanicalKeyboards/comments/4i37j0/does_anyone_know_what_color_codes_are_used_for/
+[sp]: https://deskthority.net/wiki/Signature_Plastics_ABS_colours
