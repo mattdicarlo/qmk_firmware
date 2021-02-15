3c9d4973c72aa25a46e7c861e904dbfcface9c74
Matt DiCarlo <dicarlo.matt@gmail.com>
2021-02-14 19:45:36 -0600

    Personal keymap updates

--- keyboards/id80/keymaps/mattdicarlo/keymap.c
+++ keyboards/id80/keymaps/mattdicarlo/keymap.c
@@ -1,7 +1,12 @@
 #include QMK_KEYBOARD_H
 
+enum md_layers {
+    MAC = 0,    // Base layer with Mac OS layout
+    FN,         // Function layer
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [MAC] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGDN,
@@ -9,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,
         KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    KC_RGUI, MO(1),                     KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [1] = LAYOUT(
+    [FN] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MRWD, KC_MFFD, KC_MPLY, KC_MUTE,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU,
         _______, RGB_TOG, _______, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, KC_VOLD,
@@ -19,9 +24,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
+
 void keyboard_post_init_user(void) {
 #ifdef RGBLIGHT_ENABLE
     rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
-    rgblight_sethsv(195, 0xff, 64);
+
+    // QMK uses 8bit vals for HSV, so we need to convert them from
+    // 360º hue and percentage sat and val.
+    rgblight_sethsv((266.0 / 360.0) * 255,
+                    0.47 * 255,
+                    0.49 * 255);
+    //rgblight_sethsv((169.0 / 360.0) * 255,
+    //                0.55 * 255,
+    //                0.81 * 255);
 #endif //RGBLIGHT_ENABLE
+}
+
+
+uint32_t layer_state_set_user(uint32_t state) {
+#ifdef RGBLIGHT_ENABLE
+    switch (biton32(state)) {
+        case MAC:
+            rgblight_sethsv((266.0 / 360.0) * 255,
+                    0.47 * 255,
+                    0.49 * 255);
+
+            break;
+        case FN:
+            rgblight_sethsv((169.0 / 360.0) * 255,
+                            0.55 * 255,
+                            0.81 * 255);
+            break;
+
+        default:
+            break;
+    }
+#endif // RGBLIGHT_ENABLE
+
+    return state;
 }
---
+++ keyboards/massdrop/alt/keymaps/mattdicarlo-win/keymap.c
@@ -0,0 +1,166 @@
+#include QMK_KEYBOARD_H
+
+enum alt_keycodes {
+    U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
+    U_T_AGCR,              //USB Toggle Automatic GCR control
+    DBG_TOG,               //DEBUG Toggle On / Off
+    DBG_MTRX,              //DEBUG Toggle Matrix Prints
+    DBG_KBD,               //DEBUG Toggle Keyboard Prints
+    DBG_MOU,               //DEBUG Toggle Mouse Prints
+    MD_BOOT,               //Restart into bootloader after hold timeout
+};
+
+#define TG_NKRO MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
+
+keymap_config_t keymap_config;
+
+enum md_layers {
+    MAC = 0,    // Base layer with Windows layout
+    FN,         // Momentary layer overlaid on Windows layer
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [MAC] = LAYOUT(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
+        KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(FN),  KC_LEFT, KC_DOWN, KC_RGHT  \
+    ),
+    [FN] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_MUTE, \
+        _______, _______, _______, _______, _______, _______, _______, U_T_AUTO,U_T_AGCR,_______, _______, _______, _______, _______, KC_END, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
+        _______, RGB_HUD, RGB_HUI, RGB_TOG, _______, _______, _______, _______, RGB_VAD, RGB_VAI, _______, _______,          KC_PGUP, KC_VOLD, \
+        _______, _______, KC_MNXT,                            KC_MPLY,                            KC_MPRV, _______, KC_HOME, KC_PGDN, KC_END  \
+    )
+    /*
+    [X] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
+    ),
+    */
+};
+
+#ifdef RGB_MATRIX_ENABLE
+// Access to the current RGB state.
+extern rgb_config_t rgb_matrix_config;
+
+// Set the first time we activate one of the layer effects.
+bool md_rgb_initialized = false;
+uint8_t md_rgb_prev_val = 120;
+#endif // RGB_MATRIX_ENABLE
+
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+};
+
+
+void keyboard_post_init_user(void) {
+#ifdef RGB_MATRIX_ENABLE
+    rgb_matrix_enable();
+    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv(0, 0, 0);
+
+    // Toggle once to set the lighting mode to just the keyswitches without underglow.
+    // Gotta be a better way to do this.
+    //rgb_matrix_toggle();
+#endif // RGB_MATRIX_ENABLE
+};
+
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+};
+
+
+#define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
+#define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
+#define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint32_t key_timer;
+
+    switch (keycode) {
+        case U_T_AUTO:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
+            }
+            return false;
+        case U_T_AGCR:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+            }
+            return false;
+        case DBG_TOG:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+            }
+            return false;
+        case DBG_MTRX:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+            }
+            return false;
+        case DBG_KBD:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+            }
+            return false;
+        case DBG_MOU:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+            }
+            return false;
+        case MD_BOOT:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+            } else {
+                if (timer_elapsed32(key_timer) >= 500) {
+                    reset_keyboard();
+                }
+            }
+            return false;
+        default:
+            return true; //Process all other keycodes normally
+    }
+}
+
+
+uint32_t layer_state_set_user(uint32_t state) {
+#ifdef RGB_MATRIX_ENABLE
+    switch (biton32(state)) {
+        case MAC:
+            if (md_rgb_initialized) {
+                md_rgb_prev_val = rgb_matrix_config.val;
+            }
+            rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
+            rgb_matrix_sethsv(0, 0, 0);
+
+            //rgb_matrix_set_color(1, 0xff, 0x00, 0x00);
+            break;
+        case FN:
+            rgb_matrix_mode(RGB_MATRIX_SOLID_MULTISPLASH);
+            rgb_matrix_sethsv(195, 0xff, md_rgb_prev_val);
+            md_rgb_initialized = true;
+            //rgb_matrix_set_color(1, 0xff, 0x00, 0x00);
+            break;
+
+        default:
+            break;
+    }
+#endif // RGB_MATRIX_ENABLE
+
+    return state;
+}
+
+
+#ifdef RGB_MATRIX_ENABLE
+//void rgb_matrix_indicators_user(void) {
+//    rgb_matrix_set_color(0, 0xff, 0x00, 0x00);
+//}
+#endif // RGB_MATRIX_ENABLE
